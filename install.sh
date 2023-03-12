3544891b3c2b72ab835a9974f520afd43cd5c5da
twojstaryzdomu <twojstaryzdomu@github.com>
2023-03-12 17:52:26 +0100

    Add REINIT env var to re-initialise the configuration from scratch, dropping NC database & user environment

--- install.sh
+++ install.sh
@@ -26,11 +26,16 @@ export PATH="/usr/local/sbin:/usr/sbin:/sbin:${PATH}"
 
 # check installed software
 type mysqld &>/dev/null && echo ">>> WARNING: existing mysqld configuration will be changed <<<"
-type mysqld &>/dev/null && mysql -e 'use nextcloud' &>/dev/null && { echo "The 'nextcloud' database already exists. Aborting"; exit 1; }
+type mysqld &>/dev/null \
+  && {
+    [ -n "${REINIT}" ] \
+      && { mysql -e 'drop database nextcloud' || :; } \
+      || { echo "The 'nextcloud' database already exists. Aborting"; exit 1; }
+  }
 
 # get dependencies
 [ -n "${NOUPDATE}" ] || apt-get update
-apt-get install --no-install-recommends -y git ca-certificates sudo lsb-release wget
+[ -n "${REINIT}" ] || apt-get install --no-install-recommends -y git ca-certificates sudo lsb-release wget
 
 # get install code
 if [[ "${CODE_DIR}" == "" ]]; then
@@ -62,11 +67,15 @@ cp etc/library.sh /usr/local/etc/
 cp etc/ncp.cfg /usr/local/etc/
 
 cp -r etc/ncp-templates /usr/local/etc/
-install_app    lamp.sh
-install_app    bin/ncp/CONFIG/nc-nextcloud.sh
+[ -n "${REINIT}" ] \
+  || {
+    install_app    lamp.sh
+    install_app    bin/ncp/CONFIG/nc-nextcloud.sh
+  }
 run_app_unsafe bin/ncp/CONFIG/nc-nextcloud.sh
 rm /usr/local/etc/ncp-config.d/nc-nextcloud.cfg    # armbian overlay is ro
 systemctl restart mysqld # TODO this shouldn't be necessary, but somehow it's needed in Debian 9.6. Fixme
+[ -n "${REINIT}" ] && rm /usr/local/etc/ncp-config.d/nc-init.cfg || :
 install_app    ncp.sh
 run_app_unsafe bin/ncp/CONFIG/nc-init.sh
 echo 'Moving data directory to a more sensible location'
