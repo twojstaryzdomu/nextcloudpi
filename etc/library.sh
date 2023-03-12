037ae32eb4a004c1b3e607254fe17bc4c7222e8a
twojstaryzdomu <twojstaryzdomu@github.com>
2023-03-12 17:42:59 +0100

    Fix redis credentials

--- etc/library.sh
+++ etc/library.sh
@@ -13,6 +13,7 @@ export BINDIR=/usr/local/bin/ncp
 export NCDIR=/var/www/nextcloud
 export ncc=/usr/local/bin/ncc
 export NCPCFG=${NCPCFG:-etc/ncp.cfg}
+export REDIS_CONF=${REDIS_CONF:-/etc/redis/redis.conf}
 export APACHE_LOG_DIR=${APACHE_LOG_DIR:-/var/log/apache2}
 export ARCH="$(dpkg --print-architecture)"
 [[ "${ARCH}" =~ ^(armhf|arm)$ ]] && ARCH="armv7"
